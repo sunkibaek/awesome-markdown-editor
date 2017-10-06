7b02985081922831e18b6745ba80e2ec60128c2f
Sunki Baek <sunki.baek@gmail.com>
2017-10-06 15:05:20 +0900

    js 상에서 css를 로딩할 수 있도록 도와주는 웹팩 플러그인 추가

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,11 @@ module.exports = {
         enforce: "pre",
         test: /\.js$/,
         loader: "source-map-loader"
+      },
+
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
