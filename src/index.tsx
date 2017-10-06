0d8e22aabbfc09d8fdda9dc654b785e1d7359438
Sunki Baek <sunki.baek@gmail.com>
2017-10-06 16:03:16 +0900

    앱 전체 스타일링에 사용될 main.css 파일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./css/main.css";
+
 const App = () => <h1>Awesome Markdown Editor</h1>;
 
 ReactDOM.render(<App />, document.getElementById("container"));
