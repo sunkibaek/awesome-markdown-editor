e298a5095988423af401c7b0b349ee52a265e647
Sunki Baek <sunki.baek@gmail.com>
2017-10-06 16:26:10 +0900

    앱 내에서 공통으로 쓰이는 스타일 관련 상수 추출

---
+++ src/styles/Color.ts
@@ -0,0 +1,7 @@
+class Color {
+  public static WHITE = "#F7F8FA";
+  public static LIGHT_GRAY = "#CED2D9";
+  public static DARK_GRAY = "#767E89";
+}
+
+export default Color;
---
+++ src/styles/Space.ts
@@ -0,0 +1,6 @@
+class Space {
+  public static md = 10;
+  public static lg = 20;
+}
+
+export default Space;
